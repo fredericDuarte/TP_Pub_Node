ce8ff8a199fd5324dc346ed433cb99da6fc96f4e
Frederic DUARTE <geforst@gmail.com>
2019-03-21 18:09:05 +0100

    pub rest api todo

---
+++ pub-rest-api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const pubService = require('pub-services').services.pubService;
+const app = express();
+app.get('/pubs', function (req, res) {
+   // const pubs = pubService
+
+    res.status(200).json();
+})
+var server = app.listen(3000, function () {
+    console.log('Listening on port %d', server.address().port);
+});
